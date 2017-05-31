c382922dadde007faaaae30d2b707d7d3fa9e07a
arzyu <arzyu@live.cn>
2017-05-31 17:34:21 +0800

    handle component click

--- src/components/painter-tree/recursivelyAddId.js
+++ src/components/painter-tree/recursivelyAddId.js
@@ -3,8 +3,9 @@ import { v4 } from 'uuid';
 export default function recursivelyAddId(nodes) {
 	const queue = [...nodes];
 	let node;
-	while (node = queue.shift()) { // eslint-disable-line
+	while (node = queue.shift()) { // eslint-disable-line no-cond-assign
 		node.cid = v4();
+		node['@click.native.stop'] = `__onFocus('${node.cid}')`;
 		if (node.children) {
 			queue.push(...node.children || []);
 		}
